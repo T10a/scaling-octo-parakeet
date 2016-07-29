1849293b875b7f72f86503fdf580fa504c11a6ef
t10a <thomasccoote@gmail.com>
2016-07-29 16:26:11 +1000

    Update to 1.10.2

--- src/main/java/com/t10a/tutorial/Reference.java
+++ src/main/java/com/t10a/tutorial/Reference.java
@@ -4,8 +4,8 @@ public class Reference
 {
 	public static final String MOD_ID = "tutorial";
 	public static final String NAME = "1.9 Tutorial Mod";
-	public static final String VERSION = "1.9.4-1.0";
-	public static final String ACCEPTED_VERSION = "[1.9.4]";
+	public static final String VERSION = "1.10.2-1.0";
+	public static final String ACCEPTED_VERSION = "[1.9.4,1.10.2]";
 	
 	public static final String CLIENT_PROXY_CLASS = "com.t10a.tutorial.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.t10a.tutorial.proxy.ServerProxy";
